ca63a2f6c7e662574ae4199827c9148a6852dcc4
unknown <you@example.com>
2023-06-12 10:42:12 +0530

    added loading button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,14 @@ import axios from "axios";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Badge from 'react-bootstrap/Badge';
+import Spinner from 'react-bootstrap/Spinner';
 const apiURL = "https://fileupload01.up.railway.app";
 
 function App() {
   const [selectedFile, setSelectedFile] = useState(null);
   const [isUploaded, setIsUploaded] = useState(false);
   const [downloadLink, setDownloadLink] = useState(null);
+  const [isLoading, setLoading] = useState(false);
 
   const handleFileChange = (event) => {
     setSelectedFile(event.target.files[0]);
@@ -19,16 +21,18 @@ function App() {
     event.preventDefault();
     try {
       if (selectedFile) {
-      const formData = new FormData();
-      formData.append('doc', selectedFile);
-      let responseData = await axios({
-        method: "post",
-        url: `${apiURL}/file`,
-        data: formData,
-      });
-      setIsUploaded(true);
-      setDownloadLink(responseData.data.fileLink);
-      setSelectedFile(null)
+        setLoading(true);
+        const formData = new FormData();
+        formData.append('doc', selectedFile);
+        let responseData = await axios({
+          method: "post",
+          url: `${apiURL}/file`,
+          data: formData,
+        });
+        setIsUploaded(true);
+        setDownloadLink(responseData.data.fileLink);
+        setSelectedFile(null)
+        setLoading(false);
       }
     } catch (error) {
       console.log(error);
@@ -46,12 +50,18 @@ function App() {
         <Form.Group>
           <Form.Label>Select File</Form.Label>
           <Form.Control className='w-50' type="file" onChange={handleFileChange} />
-          <Form.Text className="text-muted">
+          {!selectedFile && <Form.Text className="text-muted">
             Please select a file to upload.
-          </Form.Text>
+          </Form.Text>}
         </Form.Group>
-        <Button variant="primary" type="submit">
-          Upload
+        <Button variant="primary" type="submit" className='mt-2'>
+          {isLoading ? <> <Spinner
+            as="span"
+            animation="grow"
+            size="sm"
+            role="status"
+            aria-hidden="true"
+          /> Loading...</> : 'Upload'}
         </Button>
       </Form>
 
